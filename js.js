8770c4053d1ab071aab721859728f04ee1e216b7
nafizsheikh12 <nafizsheikh394@gmail.com>
2021-10-28 12:35:55 +0600

    new commit

--- js.js
+++ js.js
@@ -1,33 +1,21 @@
-const plus = document.querySelector('.plus')
-const itemAdd = document.querySelector('.item_add')
-const submit = document.querySelector('#submit')
-const title = document.querySelector('#title')
-const code = document.querySelector('#code')
-const writername = document.querySelector('#writername')
-const list = document.querySelector('.list')
-const add = document.querySelector('.add')
 
-console.log(title)
-plus.addEventListener("click",() => {
-  itemAdd.classList.toggle('hidebb')
-})
+const list = document.querySelector('.list')
+const add = document.querySelector('.plus')
+
+
 
 
 add.addEventListener('click',(e) => {
     e.preventDefault();
-
-    if(title.value == "" && writername.value == "" && writername.value == ""){
-        alert("bhai kisu den")
-    }else{
     const row = document.createElement('div');
     row.innerHTML = `
      <li class="table-row">
-          <div class="col col-1">${title.value}</div>
-          <div class="col col-2">${code.value}</div>
-          <div class="col col-3">${writername.value}</div>
-          <div class="col col-4">N/A</div>
+          <div class="col col-1"><input placeholder="Persepolis"/></div>
+          <div class="col col-2"><input placeholder="543"/></div>
+          <div class="col col-3"><input placeholder="Mark Haddon"/></div>
+          <div class="col col-4"><input placeholder="N/A"/></div>
     </li>
     `
    list.appendChild(row)
-    }
+    
 })
